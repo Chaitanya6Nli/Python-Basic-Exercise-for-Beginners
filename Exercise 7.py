6ce478c5b2b1e8428c3821b8e80df6f9b65e818d
Chaitanya6Nli <chaitanyabelekar59@gmail.com>
2025-02-25 19:10:58 +0530

    Create Exercise 7.py
     Count occurrences of a substring in a string

---
+++ Exercise 7.py
@@ -0,0 +1,13 @@
+# Find the number of occurences of a substring in a string
+
+'''
+Write a Python code to find how often the substring "Emma" appears in the given string:
+
+"Emma is good developer. Emma is a writer"
+
+expected output: 2
+'''
+
+str_x = "Emma is good developer. Emma is a writer"
+print(str_x.count("Emma"))
+
