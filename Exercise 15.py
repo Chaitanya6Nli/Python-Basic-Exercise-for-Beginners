ce485bc7170a12be0e3737b0f48fe17f3133247d
Chaitanya6Nli <chaitanyabelekar59@gmail.com>
2025-02-26 20:13:39 +0530

    Create Exercise 15.py
    Get an int value of base raises to the power of exponent

---
+++ Exercise 15.py
@@ -0,0 +1,12 @@
+# Get an int value of base raises to the power of another int value of exponent
+
+'''
+Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+Note: here exp is a non-negative integer, and base is an integer.
+'''
+
+def exponent(base, exp):
+    return base ** exp
+
+print(exponent(2, 3))
